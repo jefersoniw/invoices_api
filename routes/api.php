6ab31abee22da93d40f8043a9d2e84a8c4b8eab7
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2023-09-21 23:51:58 -0300

    Usando resource para lista de rotas.

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,11 @@ Route::prefix('/v1')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{user}', [UserController::class, 'show']);
 
-    Route::get('/invoices', [InvoiceController::class, 'index']);
-    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
-    Route::post('/invoices', [InvoiceController::class, 'store']);
-    Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
-    Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
+    Route::apiResource('invoices', InvoiceController::class);
+
+    // Route::get('/invoices', [InvoiceController::class, 'index']);
+    // Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
+    // Route::post('/invoices', [InvoiceController::class, 'store']);
+    // Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
+    // Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
 });
